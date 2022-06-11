f269d3f052c35508a823bf96769d20ad887ae28f
Jeroen Venema <jeroen.venema@gmail.com>
2022-06-11 18:33:26 +0200

    Crystal game for z80 in progress

---
+++ crystalmatch/console.h
@@ -0,0 +1,20 @@
+#ifndef   CONSOLE_H
+#define   CONSOLE_H
+
+#define SCREENWIDTH  40
+#define SCREENHEIGHT 30
+#define BIOS_OUTBOXFLAG     (unsigned char*)0x0200
+#define BIOS_OUTBOXDATA     (unsigned char*)0x0201
+#define VIDEOSTART          (unsigned char*)0xf800
+#define VIDEOEND            (VIDEOSTART + SCREENWIDTH*SCREENHEIGHT)
+
+typedef volatile unsigned char chardefs[8];             // use as pointer to character definition memory area (0xf000)
+                                                        // ex: chardefs *ptr = (chardefs *)0xf000;
+void con_init();                                        // needed before any console IO
+void con_gotoxy(unsigned char x, unsigned char y);
+void con_cls();
+void con_putc(char c);
+void con_puts(const char *s);
+char con_getc();                                        // blocked wait
+
+#endif
